c2363c460ef6f42b6f741f55de0ac963c7e50b5f
FauconFan <joseph0priou@gmail.com>
2017-10-02 16:44:25 +0200

    dellete ft strconcat... already exists with ftjoin

--- libft/variables_files.mk
+++ libft/variables_files.mk
@@ -133,8 +133,6 @@ MyBONUS = 	$(SRCS_FOLDER)$(MYBONUS_FOLDER)ft_strpos.c \
 			$(SRCS_FOLDER)$(MYBONUS_FOLDER)ft_strrpos.c \
 			$(SRCS_FOLDER)$(MYBONUS_FOLDER)ft_strcpos.c \
 			$(SRCS_FOLDER)$(MYBONUS_FOLDER)ft_strcrpos.c \
-			$(SRCS_FOLDER)$(MYBONUS_FOLDER)ft_strconcat.c \
-			$(SRCS_FOLDER)$(MYBONUS_FOLDER)ft_strconcat_n_free.c \
 			$(SRCS_FOLDER)$(MYBONUS_FOLDER)ft_strparse.c \
 			$(SRCS_FOLDER)$(MYBONUS_FOLDER)ft_strparse_n_free.c \
 			$(SRCS_FOLDER)$(MYBONUS_FOLDER)ft_putnbrl.c
@@ -142,8 +140,6 @@ MyBONUS_OBJ = 	$(BIN_FOLDER)ft_strpos.o \
 				$(BIN_FOLDER)ft_strrpos.o \
 				$(BIN_FOLDER)ft_strcpos.o \
 				$(BIN_FOLDER)ft_strcrpos.o \
-				$(BIN_FOLDER)ft_strconcat.o \
-				$(BIN_FOLDER)ft_strconcat_n_free.o \
 				$(BIN_FOLDER)ft_strparse.o \
 				$(BIN_FOLDER)ft_strparse_n_free.o \
 				$(BIN_FOLDER)ft_putnbrl.o
