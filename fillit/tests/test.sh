717bd20d06323bff607b830af50569c161cba61f
fauconfan <joseph0priou@gmail.com>
2017-11-09 14:06:44 +0100

    delete tests fillit

--- fillit/tests/test.sh
+++
@@ -1,9 +0,0 @@
-
-if [[ $# != "1" ]]; then
-	exit 1
-fi
-/home/fauconfan/Documents/Tetriminos-generator/tetri-gen -i -f 15
-cat sample.fillit > $1.txt
-rm -f sample.fillit
-../fillit $1.txt > $1.ans.txt
-cat $1.ans.txt
