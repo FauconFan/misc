22ecdc540d77a311378fb3e8ba70259fbf45cdbe
Joseph PRIOU <jpriou@e2r6p17.42.fr>
2017-11-25 09:01:49 +0100

    make some modification for gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jpriou <jpriou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/12 13:42:36 by jpriou            #+#    #+#             */
-/*   Updated: 2017/11/24 09:32:44 by jpriou           ###   ########.fr       */
+/*   Updated: 2017/11/25 08:53:45 by jpriou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,35 @@ static void		fill_line(char **line, char **buff_prog)
 {
 	char	*tmp;
 	int		i;
+	int		size;
 
 	i = 0;
-	tmp = NULL;
+	tmp = 0;
 	while ((*buff_prog)[i] != '\n' && (*buff_prog)[i])
 		i++;
 	*line = ft_strsub(*buff_prog, 0, i);
-	if (ft_strlen(*buff_prog) > 0)
-		tmp = ft_strsub(*buff_prog, i + 1, ft_strlen(*buff_prog) - i);
+	size = 0;
+	while ((*buff_prog)[i + size] != 0)
+		size++;
+	tmp = ft_strsub(*buff_prog, i + 1, size);
 	free(*buff_prog);
 	*buff_prog = tmp;
 }
 
-static int		loop(const int fd, char *buff_prog[MAX_FD], int ret, int new_buff_size)
+static int		loop(const int fd, char *buff_prog[MAX_FD])
 {
 	char	*tmp;
 	char	*buff;
+	int		ret;
 
-	if ((buff = (char *)malloc(sizeof(char) * (new_buff_size + 1))) == 0)
-		return (-1);
+	buff = 0;
+	ret = 0;
+	if (ft_strchr(buff_prog[fd], '\n') == 0)
+		buff = ft_strnew(BUFF_SIZE + 1);
 	while (ft_strchr(buff_prog[fd], '\n') == 0)
 	{
-		ft_bzero(buff, new_buff_size + 1);
-		if ((ret = read(fd, buff, new_buff_size)) == -1)
+		ft_bzero(buff, BUFF_SIZE + 1);
+		if ((ret = read(fd, buff, BUFF_SIZE)) == -1)
 			break ;
 		if (ret == 0)
 			break ;
@@ -46,32 +52,25 @@ static int		loop(const int fd, char *buff_prog[MAX_FD], int ret, int new_buff_si
 		tmp = ft_strjoin(buff_prog[fd], buff);
 		free(buff_prog[fd]);
 		buff_prog[fd] = tmp;
-		if (ft_strlen(buff) < new_buff_size)
+		if (ft_strlen(buff) < BUFF_SIZE)
 			break ;
 	}
-	free(buff);
+	if (buff != 0)
+		free(buff);
 	return ((ret == -1) ? -1 : 0);
-}
-
-static int		actualize_buff_size()
-{
-	if (BUFF_SIZE < 1)
-		return (1);
-	return (BUFF_SIZE);
 }
 
 int				get_next_line(const int fd, char **line)
 {
 	static char		*buff_prog[MAX_FD];
-	int				ret;
 
-	if (fd < 0 || line == 0 || fd > 4000 || read(fd, buff_prog[1], 0) < 0)
+	if (line == 0 || read(fd, 0, 0) < 0)
 		return (-1);
 	if (buff_prog[fd] == 0)
 		buff_prog[fd] = ft_strnew(1);
-	if (loop(fd, buff_prog, ret, actualize_buff_size()) == -1)
+	if (loop(fd, buff_prog) == -1)
 		return (-1);
-	if (ft_strcmp(buff_prog[fd], "") == 0)
+	if (*(buff_prog[fd]) == 0)
 		return (0);
 	fill_line(line, &buff_prog[fd]);
 	return (1);
