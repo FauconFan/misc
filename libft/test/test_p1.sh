ae2e37d4dec93a138b06ea05fdae580f770f2c2b
Joseph PRIOU <jpriou@e2r6p17.42.fr>
2017-12-05 10:18:01 +0100

    make libft great again

--- libft/test/test_p1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-gcc -L../ -lft main_p1.1.c -o tmp/exec1_p1 -I../
-gcc  main_p1.2.c -o tmp/exec2_p1 -I../
-./tmp/exec1_p1 > tmp/tmp1.txt
-./tmp/exec2_p1 > tmp/tmp2.txt
-diff -a tmp/tmp1.txt tmp/tmp2.txt
--- libft/test/test_p2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-gcc main_p2.c -o tmp/exec_p2 -I../ -L../ -lft
-./tmp/exec_p2
