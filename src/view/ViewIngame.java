9186a479c0b17dfbd61e9a1931571e83ef58712e
leethater <budzkoal@gmail.com>
2018-02-02 10:47:53 +0100

    Ingame suite

--- src/view/ViewIngame.java
+++ src/view/ViewIngame.java
@@ -1,5 +1,8 @@
+package src.view;
+
 import javafx.application.Application;
 import javafx.scene.Group;
+import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.Parent;
@@ -12,7 +15,6 @@ import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 
-import javafx.stage.Stage;
 
 
 public class ViewIngame extends Scene
@@ -21,9 +23,10 @@ public class ViewIngame extends Scene
 
 	public ViewIngame()
 	{
-		cam = new PerspectiveCamera(true);
+		super(createContent());
+
 		this.addEventHandler(KeyEvent.KEY_PRESSED, (key)->{
-			if (key.getCode() == KeyCode.Q)
+			if (key.getCode() == KeyCode.LEFT)
 			{
 				System.out.println("You pressed left");
 			}
@@ -40,6 +43,28 @@ public class ViewIngame extends Scene
 				System.out.println("You pressed backward");
 			}
 		});
-		this.setCamera(cam);
+	}
+
+	public Parent createContent() throws Exception
+	{
+		// Create and position camera
+		PerspectiveCamera camera = new PerspectiveCamera(true);
+
+		camera.getTransforms().addAll(
+			new Rotate(-20, Rotate.Y_AXIS),
+			new Rotate(-20, Rotate.X_AXIS),
+			new Translate(0, 0, -15));
+
+		// Build the Scene Graph
+		Group root = new Group();
+		root.getChildren().add(camera);
+
+		// Use a SubScene
+		SubScene subScene = new SubScene(root, 300, 300);
+		subScene.setFill(Color.ALICEBLUE);
+		subScene.setCamera(camera);
+		Group group = new Group();
+		group.getChildren().add(subScene);
+		return (group);
 	}
 }
