3e532ca896cb3b186df485fd966fd4df7c8dc359
Alexandre Moine <nobrakal@cthugha.org>
2018-02-06 20:42:34 +0100

    Real file

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -5,14 +5,18 @@ import src.model.gen.AlgoSample;
 import src.model.MainMaze;
 import src.view.View;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import com.google.gson.Gson;
+
+import java.io.PrintWriter;
 
 public class Controller
 {
@@ -62,36 +66,46 @@ public class Controller
 
 		if (format != null)
 		{
-			ObjectInputStream ois = null;
-			try{
-				ois = new ObjectInputStream(new FileInputStream(path));
-				if (format == SavesFormat.serialized)
-				{
+			switch (format)
+			{
+			case serialized:
+				ObjectInputStream ois = null;
+				try{
+					ois       = new ObjectInputStream(new FileInputStream(path));
 					this.maze = (MainMaze)ois.readObject();
+				} catch (final ClassNotFoundException e) {
+					this.view.setMsg("ERROR: " + e);
+				} catch (final java.io.InvalidClassException e) {
+					e.printStackTrace();
+				}  catch (final java.io.IOException e) {
+					e.printStackTrace();
+					this.view.setMsg("ERROR: IO Exception");
 				}
-				else
-				{
-					Gson gson = new Gson();
-					this.maze = gson.fromJson((String)ois.readObject(), MainMaze.class);
-				}
-			} catch (final ClassNotFoundException e) {
-				this.view.setMsg("ERROR: " + e);
-			} catch (final java.io.InvalidClassException e) {
-				e.printStackTrace();
-			}  catch (final java.io.IOException e) {
-				e.printStackTrace();
-				this.view.setMsg("ERROR: IO Exception");
-			}
-
-			finally {
-				try {
-					if (ois != null)
-					{
-						ois.close();
+				finally {
+					try {
+						if (ois != null)
+						{
+							ois.close();
+						}
+					} catch (final IOException ex) {
+						ex.printStackTrace();
 					}
-				} catch (final IOException ex) {
-					ex.printStackTrace();
 				}
+				break;
+
+			case json:
+				try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+					String sCurrentLine;
+					String text = "";
+					while ((sCurrentLine = br.readLine()) != null)
+					{
+						text += sCurrentLine;
+					}
+					this.maze = (new Gson()).fromJson(text, MainMaze.class);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				break;
 			}
 		}
 		else
@@ -113,31 +127,36 @@ public class Controller
 		{
 			path += "." + format.name() + "." + EXTENSION;
 		}
-		try {
-			oos = new ObjectOutputStream(new FileOutputStream(path));
-			switch (format)
-			{
-			case serialized:
-				oos.writeObject(this.maze);
-				break;
-
-			case json:
-				final Gson gson = new Gson();
-				oos.writeObject(gson.toJson(this.maze));
-			}
-			oos.flush();
-		} catch (final java.io.IOException e) {
-			e.printStackTrace();
-		} finally {
+		switch (format)
+		{
+		case serialized:
 			try {
-				if (oos != null)
-				{
-					oos.flush();
-					oos.close();
+				oos = new ObjectOutputStream(new FileOutputStream(path));
+				oos.writeObject(this.maze);
+				oos.flush();
+			} catch (final java.io.IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (oos != null)
+					{
+						oos.flush();
+						oos.close();
+					}
+				} catch (final IOException ex) {
+					ex.printStackTrace();
 				}
-			} catch (final IOException ex) {
-				ex.printStackTrace();
 			}
+			break;
+
+		case json:
+			try{ PrintWriter out = new PrintWriter(path);
+				 out.println((new Gson()).toJson(this.maze));
+				 out.close(); }
+			catch (final java.io.FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			break;
 		}
 	}
 
@@ -156,5 +175,15 @@ public class Controller
 			}
 		}
 		return (null);
+	}
+
+	// TEST
+	public static void main(String[] args)
+	{
+		Controller con = new Controller();
+
+		con.createMaze(new AlgoSample(), 30);
+		con.saveMaze("./ser", SavesFormat.json);
+		con.loadMaze(new java.io.File("./ser.json.maze"));
 	}
 }
