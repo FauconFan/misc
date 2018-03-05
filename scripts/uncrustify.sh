cc3f3ee66d0b324b5144574b524fa86cd70eec95
fauconfan <joseph0priou@gmail.com>
2018-03-05 21:32:05 +0100

    shellcheck unscrutify.sh

--- scripts/uncrustify.sh
+++ scripts/uncrustify.sh
@@ -5,18 +5,18 @@ G_PATH_CONFIG="scripts/config_uncrustify.cfg"
 
 TMP_FILE="TMP_REMOVE_ME.txt"
 
-if [ ! -f $G_PATH_PATH_UNCRUSTIFY ]; then
-	echo "The file $G_PATH_PATH_UNCRUSTIFY may not exist."
+if [ ! -f ${G_PATH_PATH_UNCRUSTIFY} ]; then
+	echo "The file ${G_PATH_PATH_UNCRUSTIFY} may not exist."
 	echo "You have to create it and add the path to the uncrustify executable"
 	echo "Look the repository uncrustify on Github to compile it"
 else
-	ret=`find ./src/ -name "*.java"`
-	path_unc=`cat $G_PATH_PATH_UNCRUSTIFY`
-	echo $path_unc
+	ret=$(find ./src/ -name "*.java")
+	PATH_UNC=$(cat ${G_PATH_PATH_UNCRUSTIFY})
+	echo "${PATH_UNC}"
 	for elem in $ret; do
-		$path_unc -c $G_PATH_CONFIG --replace --no-backup --mtime $elem
-		cat ${elem} | sed 's/ : : /::/g' > ${TMP_FILE}
-		cp ${TMP_FILE} ${elem}
+		${PATH_UNC} -c ${G_PATH_CONFIG} --replace --no-backup --mtime "${elem}"
+		sed 's/ : : /::/g' < "${elem}" > ${TMP_FILE}
+		cp "${TMP_FILE}" "${elem}"
 	done
 	rm -f ${TMP_FILE}
 	exit 0
