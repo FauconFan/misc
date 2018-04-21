3588885e289003fe429613e0fbcf0829bedfa978
fauconfan <joseph0priou@gmail.com>
2018-04-21 18:03:38 +0200

    first working huffman encode

--- main.go
+++ main.go
@@ -5,9 +5,26 @@ import (
 	"io/ioutil"
 )
 
+func translate(data string, table map[rune]string) string {
+	res := ""
+	spli := []rune(data)
+	for _, c := range spli {
+		res += table[c]
+	}
+	return res
+}
+
 func main () {
-	pathToFile := "assets/lorem1.txt"
+	pathToFile := "assets/simple.txt"
 	
-	data, _ := ioutil.ReadFile(pathToFile)
-	fmt.Println(string(data))
+	data, err := ioutil.ReadFile(pathToFile)
+	if err != nil {
+		fmt.Printf("Something strange occured : %s\n", err)
+		return 
+	}
+	content := string(data)
+	table := GetAssociatedTab(content)
+
+	translated := translate(content, table)
+	fmt.Printf("%s\n", translated)
 }
