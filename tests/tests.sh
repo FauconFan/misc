b76a198f65e3e87194b48f686f8ca78b804ba9bf
fauconfan <joseph0priou@gmail.com>
2018-10-18 11:06:55 +0200

    add random test

--- tests/tests.sh
+++ tests/tests.sh
@@ -3,9 +3,12 @@
 set -e
 
 CMD="python3 ./main.py"
+CMD_GEN="python2 tests/npuzzle-gen.py"
 
 SOLUBLE_FOLDER="tests/soluble/"
 NON_SOLUBLE_FOLDER="tests/non_soluble/"
+
+PUZ_RAN_PATH="puzzle_random.txt"
 
 function test_soluble()
 {
@@ -25,6 +28,24 @@ function test_non_soluble()
 	done
 }
 
+function test_random()
+{
+	while true ; do
+		rm -f ${PUZ_RAN_PATH}
+		N=$((RANDOM % 3 + 3))
+		${CMD_GEN} $N > ${PUZ_RAN_PATH}
+		printf "Testing size %d, " $N
+		if grep unsolvable ${PUZ_RAN_PATH} > /dev/null 2>&1; then
+			printf "unsolvable "
+			${CMD} ${PUZ_RAN_PATH} | grep False > /dev/null 2>&1
+		else
+			printf "  solvable "
+			${CMD} ${PUZ_RAN_PATH} | grep True > /dev/null 2>&1
+		fi
+		printf "OK\\n"
+	done
+}
+
 if [ ! $# = 1 ]; then
 	exit 1
 fi
@@ -35,6 +56,9 @@ case $1 in
 		;;
 	non_soluble )
 		time test_non_soluble
+		;;
+	random )
+		test_random
 		;;
 	* )
 		exit 1
