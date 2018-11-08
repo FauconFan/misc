eef2a7e30a6a4b3aa1ab33589ec3463e5fbf1513
fauconfan <joseph0priou@gmail.com>
2018-11-08 15:21:47 +0100

    add CLI to get config from command line

--- src/Main.ml
+++ src/Main.ml
@@ -1,3 +1,23 @@
 
+open Base
+
+let default_weight = 600
+let default_height = 600
+let default_depth = 5
+
+let weight = ref default_weight
+let height = ref default_height
+let depth = ref default_depth
+
+let opt_weight = ("-w", Arg.Set_int weight, "weight of the window")
+let opt_height = ("-h", Arg.Set_int height, "height of the window")
+let opt_depth = ("-d", Arg.Set_int depth, "depth of the bsp tree")
+
+let spec_list = [opt_weight; opt_height; opt_depth]
+
+let usage_msg = "PF5 project in honor of Mondrian"
+
 let () =
-  print_endline "Hello world"
+  Arg.parse spec_list ignore usage_msg;
+  let config = {dims = (!weight, !height); depth = !depth} in
+  ignore(config)
