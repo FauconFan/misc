668f6057c009563b1f1d3d1b00c254056e8d9382
fauconfan <joseph0priou@gmail.com>
2018-11-23 15:25:32 +0100

    make it compile again

--- src/view/graphics/CampingScene.ml
+++ src/view/graphics/CampingScene.ml
@@ -1,6 +1,6 @@
 
 
 let defaultScene config =
-  let frame = new FrameBSP.frameBSP config 0 0 in
+  let frame = new FrameBSP.frameBSP config (0, 0) in
   let layer = new SLAC.layer [frame] in
   new SLAC.scene [| layer |]
