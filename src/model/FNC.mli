dbc32c5d5ef7f341fc410aaae9b8d20d8c6ceb14
fauconfan <joseph0priou@gmail.com>
2018-12-12 20:04:21 +0100

    k_combinaison closes #28

--- src/model/FNC.mli
+++ src/model/FNC.mli
@@ -4,9 +4,4 @@ open Graphics
 
 type litt = bool * string
 
-type prop =
-  | Litt of litt
-  | Or of prop * prop
-  | And of prop * prop
-
-val prop_to_fnc : prop -> litt list list
+val k_combinaison : int -> string list -> string list list
