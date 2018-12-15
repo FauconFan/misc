8eb47bf342d73b70afb160e587c9daeb646725bf
fauconfan <joseph0priou@gmail.com>
2018-12-15 10:03:22 +0100

    je sais pas pk, je comprenais pas.... close #42

--- src/controller/Interact.ml
+++ src/controller/Interact.ml
@@ -35,6 +35,8 @@ let changeColorOpt copt =
       if List.exists (fun (_, col) -> c = col) table_of_color then actual_color := copt
     end
 
+let last_button_pressed = ref false
+
 (*
    Fonction d'interaction avec l'utilisateur :
    - 'q' pour quitter le jeu
@@ -62,15 +64,20 @@ let interact () : (coords * color option) option =
     let event =
       wait_next_event [Button_down; Poll]
     in
-    if event.button then
+    if event.button && not !last_button_pressed then
       begin
+        last_button_pressed := true;
         let mouse = (event.mouse_x, event.mouse_y)
         and screen = (size_x (), size_y ()) in
         if bounds mouse screen
         then Some (mouse, !actual_color)
         else None
       end
-    else None
+    else
+      begin
+        if not event.button then last_button_pressed := false;
+        None
+      end
   in
   check_key_pressed ();
   check_button_pressed ()
