f74f8a1b8b008363c54c3f051db1a8d36a7ba5e2
fauconfan <joseph0priou@gmail.com>
2019-01-23 15:59:40 +0100

    add features

--- learnSDL2/main.c
+++ learnSDL2/main.c
@@ -2,6 +2,8 @@
 #include <SDL.h>
 #include <stdio.h>
 
+#define MIN(x, y)	(((x) < (y)) ? (x) : (y))
+
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -9,11 +11,22 @@ const int SCREEN_HEIGHT = 480;
 const int SCREEN_WIDTH2 = 960;
 const int SCREEN_HEIGHT2 = 720;
 
+SDL_Surface		*buff_screen = NULL;
+int				wbuff_screen = 0;
+int				hbuff_screen = 0;
+
+void			init_buff_screen(int width, int height);
+void			resize_buff_screen(int new_width, int new_height);
+void			free_buff_screen();
+
 int main(void) {
 	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+	SDL_Window		*window = NULL;
+	//Pointers to the list of pixels
+	uint32_t		*pixels = NULL;
+	//two sdl_rect util
+	SDL_Rect		origin_rect1 = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
+	SDL_Rect		origin_rect2 = {0, 0, SCREEN_WIDTH2, SCREEN_HEIGHT2};
 
 	//Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -26,36 +39,73 @@ int main(void) {
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
 		}
 		else {
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
+			init_buff_screen(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 0xFF, 0xFF));
+			SDL_FillRect(buff_screen, NULL, SDL_MapRGB(buff_screen->format, 0, 0xFF, 0xFF));
 
+			if (SDL_MUSTLOCK(buff_screen))
+				SDL_LockSurface(buff_screen);
+
+			pixels = (uint32_t *)buff_screen->pixels;
 			//Put a red square
-			uint32_t	*pixels = (uint32_t *)screenSurface->pixels;
-
 			for (size_t x = 100; x < 200; x++) {
 				for (size_t y = 100; y < 200; y++) {
-					pixels[x + (y * screenSurface->w)] = SDL_MapRGB(screenSurface->format, 0xFF, 0, 0);
+					pixels[x + (y * buff_screen->w)] = SDL_MapRGB(buff_screen->format, 0xFF, 0, 0);
 				}
 			}
 
+			for (size_t x = 300; x < 400; x++) {
+				for (size_t y = 100; y < 200; y++) {
+					int dx = abs(x - 350);
+					int dy = abs(y - 150);
+					if (dx * dx + dy * dy < 50 * 50)
+						pixels[x + (y * buff_screen->w)] = SDL_MapRGB(buff_screen->format, 0, 0xFF, 0);
+				}
+			}
+
+			if (SDL_MUSTLOCK(buff_screen))
+				SDL_UnlockSurface(buff_screen);
+
 			//Update the surface
+			SDL_BlitSurface(buff_screen, &origin_rect1, SDL_GetWindowSurface(window), NULL);
 			SDL_UpdateWindowSurface(window);
 
 			//Wait one second
 			SDL_Delay(1000);
 
+			resize_buff_screen(SCREEN_WIDTH2, SCREEN_HEIGHT2);
+
 			//Resize the window
 			SDL_SetWindowSize(window, SCREEN_WIDTH2, SCREEN_HEIGHT2);
 
+			if (SDL_MUSTLOCK(buff_screen))
+				SDL_LockSurface(buff_screen);
+
+			pixels = (uint32_t *)buff_screen->pixels;
+
+			for (int x = SCREEN_WIDTH; x < SCREEN_WIDTH2; x++) {
+				for (int y = SCREEN_HEIGHT; y < SCREEN_HEIGHT2; y++) {
+					pixels[x + (y * buff_screen->w)] = SDL_MapRGB(buff_screen->format, 0, 0, 0xFF);
+				}
+			}
+
+			if (SDL_MUSTLOCK(buff_screen))
+				SDL_UnlockSurface(buff_screen);
+
+			//Update the surface
+			SDL_BlitSurface(buff_screen, &origin_rect2, SDL_GetWindowSurface(window), NULL);
+			SDL_UpdateWindowSurface(window);
+
 			//Wait one second
 			SDL_Delay(1000);
+
+			//free buffer
+			free_buff_screen();
 		}
 	}
 	//Destroy window
-	SDL_DestroyWindow(window);
+	if (window)
+		SDL_DestroyWindow(window);
 
 	//Quit SDL subsystems
 	SDL_Quit();
@@ -63,7 +113,41 @@ int main(void) {
 	return (0);
 }
 
+void		init_buff_screen(int width, int height)
+{
+	free_buff_screen();
+	buff_screen = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
+	wbuff_screen = width;
+	hbuff_screen = height;
+}
 
+void		resize_buff_screen(int new_width, int new_height)
+{
+	SDL_Surface		*tmp;
+	SDL_Rect		orig;
+
+	tmp = SDL_CreateRGBSurface(0, new_width, new_height, 32, 0, 0, 0, 0);
+	orig.x = 0;
+	orig.y = 0;
+	orig.w = MIN(wbuff_screen, new_width);
+	orig.h = MIN(hbuff_screen, new_height);
+	SDL_BlitSurface(buff_screen, &orig, tmp, NULL);
+	free_buff_screen();
+	buff_screen = tmp;
+	wbuff_screen = new_width;
+	hbuff_screen = new_height;
+}
+
+void		free_buff_screen()
+{
+	if (buff_screen != NULL)
+	{
+		SDL_FreeSurface(buff_screen);
+		buff_screen = NULL;
+		wbuff_screen = 0;
+		hbuff_screen = 0;
+	}
+}
 
 // #include <stdio.h>
 // #include <SDL2/SDL.h>
