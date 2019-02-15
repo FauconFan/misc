0f4b4c12dec3f97400742cb93b66f05437a854ea
Salin Ugo <ugo-salin@orange.fr>
2019-02-15 16:19:10 +0100

    Push avec rotate bugué

--- src/commands/cimp_rotate.c
+++ src/commands/cimp_rotate.c
@@ -19,7 +19,7 @@ void cimp_rotate(int angle){
       cimp_rotate(90);
       break;
     default:
-      printf("Nothing to do for now, invalid angle value, choose a multiple of 90°\n");
+      printf("Nothing to do   for now, invalid angle value, choose a multiple of 90°\n");
   }
 }
 void cimp_rotate90(){
@@ -27,5 +27,17 @@ void cimp_rotate90(){
   int new_width;
   int new_height;
   SDL_GetWindowSize(g_cimp->screen->window,&new_height,&new_width);
-  SDL_SetWindowSize(g_cimp->screen->window,new_height,new_width);
+  SDL_Surface *surf=SDL_CreateRGBSurface(0, new_width, new_height, 32, 0, 0, 0, 0);
+  uint32_t *old=(uint32_t *)g_cimp->screen->buff_screen->pixels;
+  uint32_t *new=(uint32_t *)surf->pixels;
+  for(int i=0;i<new_height;i++){
+    for(int j=0;j<new_width;j++){
+      new[new_width-1-j+i*new_width]=old[i+j*new_height];
+      printf("%d\n",new_width-1-j+i*new_width);
+    }
+  }
+  // libérer buff_screen
+  g_cimp->screen->buff_screen=surf;
+  SDL_SetWindowSize(g_cimp->screen->window,new_width,new_height);
+  //g_cimp->screen->buff_screen=surf;
 }
