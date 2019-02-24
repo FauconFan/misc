3997b9371136414535a23a986181c3c273cbf721
Salin Ugo <ugo-salin@orange.fr>
2019-02-24 09:49:41 +0100

    Completion fonctionnelle, il ne manque plus qu'à ce que la complétion par défaut ne s'applique qu'après la commande open et pas les autres

--- src/readline.c
+++ src/readline.c
@@ -1,12 +1,29 @@
 #include "cimp.h"
 
-int nb_completions=7;
-char *completions[]={"help","close","list","sym_verti","sym_hori","rotate","QUIT"};
-int nb_cmd=9;
+int nb_completions=0;
+char **completions;
+
+/* Fonctions internes à readline */
 char ** fileman_completion (const char *com, int start, int end);
 char *command_generator (const char *com, int num);
+char *ft_strdup(char * src);
+
+/*********************************/
+
+void initialisation_tab_completion(){
+  nb_completions=g_command_list_size;
+  completions=malloc(sizeof(char *)*nb_completions);
+  if(completions!=NULL){
+    for (size_t i=0;i<g_command_list_size;i++){
+      completions[i]=malloc(sizeof(char)*(strlen(g_command_list[i].name)+1));
+      strcpy(completions[i],g_command_list[i].name);
+    }
+  }else
+    printf("Something went terrebly wrong with readline initialisation");
+}
 
 int initialize_readline(){
+  initialisation_tab_completion();
   using_history();//initialisation de l'utilisation de la librairie history de readline
   rl_readline_name = "mon prompt>>";
 
@@ -16,35 +33,35 @@ int initialize_readline(){
 }
 
 /* com : tout ce que l'utilisateur a dÃ©jÃ  Ã©crit sur la ligne de commande
-   start, end : les indices de dÃ©but et de fin du mot sur lequel TAB a Ã©tÃ© lancÃ© */
+start, end : les indices de dÃ©but et de fin du mot sur lequel TAB a Ã©tÃ© lancÃ© */
 char ** fileman_completion (const char *com, int start, int end){
   char **matches;
   end ++;
   matches = (char **)NULL; /* assure la complÃ©tion par dÃ©faut */
 
   /* si c'est le premier mot de la ligne de commande, on l'analyse;
-     sinon on utilise l'analyse par dÃ©faut */
+  sinon on utilise l'analyse par dÃ©faut */
   if (start == 0)
-    matches = rl_completion_matches (com, command_generator);
+  matches = rl_completion_matches (com, command_generator);
 
   return (matches);
 }
 
 /* fonction qui engendre les complÃ©tions possibles :
-   Ã  chaque num >= 0 correspond une complÃ©tion;
-   la fin de la liste des complÃ©tions est donnÃ©e par NULL
+Ã  chaque num >= 0 correspond une complÃ©tion;
+la fin de la liste des complÃ©tions est donnÃ©e par NULL
 
-   ici on complÃ¨te si c'est un prÃ©fixe de "toto" ou de "turlututu",
-   sinon c'est la complÃ©tion par dÃ©faut */
+ici on complÃ¨te si c'est un prÃ©fixe de "toto" ou de "turlututu",
+sinon c'est la complÃ©tion par dÃ©faut */
 char *command_generator (const char *com, int num){
   /* Ã  l'entrÃ©e de cette fonction : com est le dÃ©but dÃ©jÃ  Ã©crit de la commande,
-     on est en train de chercher la complÃ©tion numÃ©ro num */
+  on est en train de chercher la complÃ©tion numÃ©ro num */
   static int indice, len;
   char *completion;
 
   /* si c'est la premiÃ¨re complÃ©tion qu'on cherche, on dit qu'on va chercher Ã  partir
-     de la premiere case du tableau completion et on garde en mÃ©moire la longueur
-     du texte tapÃ© */
+  de la premiere case du tableau completion et on garde en mÃ©moire la longueur
+  du texte tapÃ© */
   if (num == 0){
     indice = 0;
     len = strlen(com);
@@ -52,14 +69,27 @@ char *command_generator (const char *com, int num){
 
   /* on renvoie une complÃ©tion de prÃ©fixe le dÃ©but de la commande Ã©crite */
   while (indice < nb_completions)
-    {
-      completion = completions[indice++];
+  {
+    completion = completions[indice++];
 
-      if (strncmp (completion, com, len) == 0)
-  return completion;
-    }
+    if (strncmp (completion, com, len) == 0)
+    return ft_strdup(completion);
+  }
 
   /* est renvoyÃ© quand num est > au numÃ©ro de la derniÃ¨re complÃ©tion
-     automatique */
+  automatique */
   return NULL;
+}
+
+char *ft_strdup(char *src)
+{
+  char *str;
+  char *p;
+  int len = strlen(src);
+  str = malloc(len + 1);
+  p = str;
+  while (*src)
+  *p++ = *src++;
+  *p = '\0';
+  return str;
 }
