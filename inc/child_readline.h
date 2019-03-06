e26cd1ec246832b036a40a28d75dbfa7b9fd9ea0
fauconfan <joseph0priou@gmail.com>
2019-03-06 15:37:45 +0100

    uncrustified...

--- inc/child_readline.h
+++ inc/child_readline.h
@@ -1,9 +1,8 @@
-
 #ifndef CHILD_READLINE_H
-#define CHILD_READLINE_H
+#define	CHILD_READLINE_H
 
 void        child_send_ok();
 void        child_send_stop();
-char        *child_getline();
+char * child_getline();
 
-#endif
+#endif // ifndef CHILD_READLINE_H
