7609ce8f1452c5841d97672cf9625d8673ca99a3
Salin Ugo <ugo-salin@orange.fr>
2019-03-19 20:26:59 +0100

    test fait sur different rotate et sur tous les ases ainsi que toutes les cases de des symétries. Test de cimp_open, vérification si passe sur travis comme tel

--- tests/main_test.c
+++ tests/main_test.c
@@ -7,20 +7,45 @@ START_TEST(test_quit){
 } END_TEST
 
 START_TEST(test_rotate){
+  short truth = 1;
   SDL_Surface *surf=SDL_CreateRGBSurface(0, 3, 2, 32, 0, 0, 0, 0);
   uint32_t * new = (uint32_t *) surf->pixels;
-  new[0]=(uint32_t)32;
+  uint32_t to_test =(uint32_t)32;
+  new[0]=to_test;
   g_cimp->screen=malloc(sizeof(t_cimp_screen));
   g_cimp->screen->buff_screen=surf;
   treat_line("rotate 90");
   if(g_cimp->screen->buff_screen->w==2 && g_cimp->screen->buff_screen->h==3){
     uint32_t * pixels     = (uint32_t *) g_cimp->screen->buff_screen->pixels;
-    ck_assert(pixels[1]==(uint32_t)32);
+    for(int i=0 && truth; i<6;i++){
+      if(i==1){truth=truth && pixels[1]==to_test;}
+      else{truth=truth && pixels[i]==0;}
+    }
   }else
     ck_assert(0==1);
+  treat_line("rotate 180");
+  if(truth && g_cimp->screen->buff_screen->w==2 && g_cimp->screen->buff_screen->h==3){
+    uint32_t * pixels     = (uint32_t *) g_cimp->screen->buff_screen->pixels;
+    for(int i=0 && truth; i<6;i++){
+      if(i==4){truth=truth && pixels[4]==to_test;}
+      else{truth=truth && pixels[i]==0;}
+    }
+  }else
+    ck_assert(0==1);
+  treat_line("rotate -90");
+  if(truth && g_cimp->screen->buff_screen->w==3 && g_cimp->screen->buff_screen->h==2){
+    uint32_t * pixels     = (uint32_t *) g_cimp->screen->buff_screen->pixels;
+    for(int i=0 && truth; i<6;i++){
+      if(i==5){truth=truth && pixels[5]==to_test;}
+      else{truth=truth && pixels[i]==0;}
+    }
+  }else
+    ck_assert(0==1);
+  ck_assert(truth);
 } END_TEST
 
 START_TEST(test_sym_verti){
+  short truth = 1;
   SDL_Surface *surf=SDL_CreateRGBSurface(0, 3, 2, 32, 0, 0, 0, 0);
   uint32_t * new = (uint32_t *) surf->pixels;
   uint32_t to_test =(uint32_t)32;
@@ -30,12 +55,17 @@ START_TEST(test_sym_verti){
   treat_line("sym_verti");
   if(g_cimp->screen->buff_screen->w==3 && g_cimp->screen->buff_screen->h==2){
     uint32_t * pixels     = (uint32_t *) g_cimp->screen->buff_screen->pixels;
-    ck_assert(pixels[2]==to_test);
+    for(int i=0 && truth; i<6;i++){
+      if(i==2){truth=truth && pixels[2]==to_test;}
+      else{truth=truth && pixels[i]==0;}
+    }
+    ck_assert(truth);
   }else
     ck_assert(0==1);
 }END_TEST
 
 START_TEST(test_sym_hori){
+  short truth=1;
   SDL_Surface *surf=SDL_CreateRGBSurface(0, 3, 2, 32, 0, 0, 0, 0);
   uint32_t * new = (uint32_t *) surf->pixels;
   uint32_t to_test =(uint32_t)32;
@@ -45,7 +75,11 @@ START_TEST(test_sym_hori){
   treat_line("sym_hori");
   if(g_cimp->screen->buff_screen->w==3 && g_cimp->screen->buff_screen->h==2){
     uint32_t * pixels     = (uint32_t *) g_cimp->screen->buff_screen->pixels;
-    ck_assert(pixels[3]==to_test);
+    for(int i=0 && truth; i<6;i++){
+      if(i==3){truth=truth && pixels[3]==to_test;}
+      else{truth=truth && pixels[i]==0;}
+    }
+    ck_assert(truth);
   }else
     ck_assert(0==1);
 }END_TEST
@@ -59,9 +93,18 @@ START_TEST(test_close){
   short truth=1;
   treat_line("close");
   truth = truth && g_cimp->screen == NULL;
+  SDL_Surface *surf=SDL_CreateRGBSurface(0, 3, 2, 32, 0, 0, 0, 0);
+  g_cimp->screen=malloc(sizeof(t_cimp_screen));
+  g_cimp->screen->buff_screen=surf;
+  treat_line("close");
+  truth = truth && g_cimp->screen == NULL;
   ck_assert(truth);
 
 
+}END_TEST
+
+START_TEST(test_open){
+  treat_line("open images/untitles17.bmp");
 }END_TEST
 
 Suite *sample_suite(void) {
@@ -76,6 +119,7 @@ Suite *sample_suite(void) {
   tcase_add_test(tc_sample,test_sym_hori);
   tcase_add_test(tc_sample,empty_line);
   tcase_add_test(tc_sample,test_close);
+  tcase_add_test(tc_sample,test_open);
   suite_add_tcase(s, tc_sample);
 
   return s;
