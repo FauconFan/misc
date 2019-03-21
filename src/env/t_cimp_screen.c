d0e16fe7eb9da500075f6f11105912baa56750d8
Salin Ugo <ugo-salin@orange.fr>
2019-03-21 15:56:54 +0100

    test

--- src/env/t_cimp_screen.c
+++ src/env/t_cimp_screen.c
@@ -83,9 +83,9 @@ t_cimp_screen * cimp_init_screen(char * path_bmp) {
  */
 void                cimp_end_screen(t_cimp_screen * sc) {
 	free(sc->original_path);
-	SDL_FreeSurface(sc->buff_screen);
-	SDL_DestroyWindow(sc->window);
-	free(sc);
+	//SDL_FreeSurface(sc->buff_screen);
+	//SDL_DestroyWindow(sc->window);
+	//free(sc);
 }
 
 /**
