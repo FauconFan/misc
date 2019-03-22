b1c066f990a48a3c6147292129e8f5a1c208b547
Salin Ugo <ugo-salin@orange.fr>
2019-03-22 15:33:03 +0100

    uncrustify

--- src/commands/cimp_open.c
+++ src/commands/cimp_open.c
@@ -1,7 +1,7 @@
 #include "cimp.h"
 
 int cimp_open(t_cmd * cmd) {
-	if (g_cimp->screen == NULL && cmd!=NULL){
+	if (g_cimp->screen == NULL && cmd != NULL) {
 		g_cimp->screen = cimp_init_screen(cmd->name_file);
 		return (g_cimp->screen == NULL);
 	}
