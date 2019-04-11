31895eb6730234c283c61d2fb34fa252e53db223
Salin Ugo <ugo-salin@orange.fr>
2019-04-11 16:13:40 +0200

    uncrustify once again

--- src/commands/cimp_close.c
+++ src/commands/cimp_close.c
@@ -6,6 +6,6 @@ t_rc_cmd cimp_close(t_cmd * cmd) {
 		cimp_screen_end(g_cimp->screen[g_cimp->focus]);
 		g_cimp->screen[g_cimp->focus] = NULL;
 	}
-	g_cimp->focus=-1;
+	g_cimp->focus = -1;
 	return OK;
 }
