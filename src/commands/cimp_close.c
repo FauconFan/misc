86c15024c46d5f4d136ccfa0956fa373b37ab8a1
Salin Ugo <ugo-salin@orange.fr>
2019-04-12 17:40:11 +0200

    Correction de get_next_focus

--- src/commands/cimp_close.c
+++ src/commands/cimp_close.c
@@ -4,7 +4,7 @@ t_rc_cmd cimp_close(t_cmd * cmd) {
 	(void) cmd;
 	cimp_screen_end(g_cimp->screen[cmd->focus]);
 	g_cimp->screen[cmd->focus] = NULL;
-	if(g_cimp->focus == cmd->focus){
+	if (g_cimp->focus == cmd->focus) {
 		g_cimp->focus = get_next_focus(g_cimp->focus);
 	}
 	return OK;
