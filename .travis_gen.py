4de86702dcc78cbcc3ac521c0869f882e125e69c
fauconfan <joseph0priou@gmail.com>
2019-05-01 17:31:18 +0200

    with sudo

--- .travis_gen.py
+++ .travis_gen.py
@@ -39,7 +39,7 @@ LINT_TASKS = [
     (J_LINT, False, "uncrustify", [], ["make uncrustify_check"]),
     (J_LINT, False, "cpplint", ["python3-venv"], ["make cpplint_run"]),
     (J_LINT, False, "cppcheck", ["cppcheck"], ["make cppcheck_run"]),
-    (J_LINT, True, "clang-tidy", ["clang-tidy-6.0", "clang-6.0"], ["travis_retry make clang_tidy_run"]),
+    (J_LINT, True, "clang-tidy", ["clang-tidy-6.0", "clang-6.0"], ["make clang_tidy_run"]),
 ]
 
 # Test is a stage that test the project using the ./check_sat.py
@@ -50,16 +50,7 @@ SAT = 0
 UNSAT = 1
 
 SAT_DPLL =[
-                ("RND3SAT 01 - 09", [
-                    ("_01_RND3SAT__uf20-91", SAT),
-                    ("_02_RND3SAT__uf50-218", SAT),
-                    ("_03_RND3SAT__uuf50-218", UNSAT),
-                    ("_04_RND3SAT__uf75-325", SAT),
-                    ("_05_RND3SAT__uuf75-325", UNSAT),
-                    ("_06_RND3SAT__uf100-430", SAT),
-                    ("_07_RND3SAT__uuf100-430", UNSAT),
-                    ("_08_RND3SAT__uf125-538", SAT),
-                    ("_09_RND3SAT__uuf125-538", UNSAT),
+                ("RND3SAT 01 - 09", [("_01_RND3SAT__uf20-91", SAT), ("_02_RND3SAT__uf50-218", SAT), ("_03_RND3SAT__uuf50-218", UNSAT), ("_04_RND3SAT__uf75-325", SAT), ("_05_RND3SAT__uuf75-325", UNSAT), ("_06_RND3SAT__uf100-430", SAT), ("_07_RND3SAT__uuf100-430", UNSAT), ("_08_RND3SAT__uf125-538", SAT), ("_09_RND3SAT__uuf125-538", UNSAT),
                     # ("_10_RND3SAT__uf150-645", SAT),
                     # ("_11_RND3SAT__uuf150-645", UNSAT),
                     # ("_12_RND3SAT__uf175-753", SAT),
@@ -71,58 +62,11 @@ SAT_DPLL =[
                     # ("_18_RND3SAT__uf250-1065", SAT),
                     # ("_19_RND3SAT__uuf250-1065", UNSAT),
                 ]),
-                ("BMS 20 - 21", [
-                    ("_20_BMS__RTI_k3_n100_m429", SAT),
-                    ("_21_BMS__BMS_k3_n100_m429", SAT),
-                ]),
-                ("CBS 22 - 31", [
-                    ("_22_CBS__CBS_k3_n100_m403_b10", SAT),
-                    ("_23_CBS__CBS_k3_n100_m403_b30", SAT),
-                    ("_24_CBS__CBS_k3_n100_m403_b50", SAT),
-                    ("_25_CBS__CBS_k3_n100_m403_b70", SAT),
-                    ("_26_CBS__CBS_k3_n100_m403_b90", SAT),
-                    ("_27_CBS__CBS_k3_n100_m411_b10", SAT),
-                    ("_28_CBS__CBS_k3_n100_m411_b30", SAT),
-                    ("_29_CBS__CBS_k3_n100_m411_b50", SAT),
-                    ("_30_CBS__CBS_k3_n100_m411_b70", SAT),
-                    ("_31_CBS__CBS_k3_n100_m411_b90", SAT),
-                ]),
-                ("CBS 32 - 41", [
-                    ("_32_CBS__CBS_k3_n100_m418_b10", SAT),
-                    ("_33_CBS__CBS_k3_n100_m418_b30", SAT),
-                    ("_34_CBS__CBS_k3_n100_m418_b50", SAT),
-                    ("_35_CBS__CBS_k3_n100_m418_b70", SAT),
-                    ("_36_CBS__CBS_k3_n100_m418_b90", SAT),
-                    ("_37_CBS__CBS_k3_n100_m423_b10", SAT),
-                    ("_38_CBS__CBS_k3_n100_m423_b30", SAT),
-                    ("_39_CBS__CBS_k3_n100_m423_b50", SAT),
-                    ("_40_CBS__CBS_k3_n100_m423_b70", SAT),
-                    ("_41_CBS__CBS_k3_n100_m423_b90", SAT),
-                ]),
-                ("CBS 42 - 51", [
-                    ("_42_CBS__CBS_k3_n100_m429_b10", SAT),
-                    ("_43_CBS__CBS_k3_n100_m429_b30", SAT),
-                    ("_44_CBS__CBS_k3_n100_m429_b50", SAT),
-                    ("_45_CBS__CBS_k3_n100_m429_b70", SAT),
-                    ("_46_CBS__CBS_k3_n100_m429_b90", SAT),
-                    ("_47_CBS__CBS_k3_n100_m435_b10", SAT),
-                    ("_48_CBS__CBS_k3_n100_m435_b30", SAT),
-                    ("_49_CBS__CBS_k3_n100_m435_b50", SAT),
-                    ("_50_CBS__CBS_k3_n100_m435_b70", SAT),
-                    ("_51_CBS__CBS_k3_n100_m435_b90", SAT),
-                ]),
-                ("CBS 52 - 61", [
-                    ("_52_CBS__CBS_k3_n100_m441_b10", SAT),
-                    ("_53_CBS__CBS_k3_n100_m441_b30", SAT),
-                    ("_54_CBS__CBS_k3_n100_m441_b50", SAT),
-                    ("_55_CBS__CBS_k3_n100_m441_b70", SAT),
-                    ("_56_CBS__CBS_k3_n100_m441_b90", SAT),
-                    ("_57_CBS__CBS_k3_n100_m449_b10", SAT),
-                    ("_58_CBS__CBS_k3_n100_m449_b30", SAT),
-                    ("_59_CBS__CBS_k3_n100_m449_b50", SAT),
-                    ("_60_CBS__CBS_k3_n100_m449_b70", SAT),
-                    ("_61_CBS__CBS_k3_n100_m449_b90", SAT),
-                ]),
+                ("BMS 20 - 21", [("_20_BMS__RTI_k3_n100_m429", SAT), ("_21_BMS__BMS_k3_n100_m429", SAT)]),
+                ("CBS 22 - 31", [("_22_CBS__CBS_k3_n100_m403_b10", SAT), ("_23_CBS__CBS_k3_n100_m403_b30", SAT), ("_24_CBS__CBS_k3_n100_m403_b50", SAT), ("_25_CBS__CBS_k3_n100_m403_b70", SAT), ("_26_CBS__CBS_k3_n100_m403_b90", SAT), ("_27_CBS__CBS_k3_n100_m411_b10", SAT), ("_28_CBS__CBS_k3_n100_m411_b30", SAT), ("_29_CBS__CBS_k3_n100_m411_b50", SAT), ("_30_CBS__CBS_k3_n100_m411_b70", SAT), ("_31_CBS__CBS_k3_n100_m411_b90", SAT)]),
+                ("CBS 32 - 41", [("_32_CBS__CBS_k3_n100_m418_b10", SAT), ("_33_CBS__CBS_k3_n100_m418_b30", SAT), ("_34_CBS__CBS_k3_n100_m418_b50", SAT), ("_35_CBS__CBS_k3_n100_m418_b70", SAT), ("_36_CBS__CBS_k3_n100_m418_b90", SAT), ("_37_CBS__CBS_k3_n100_m423_b10", SAT), ("_38_CBS__CBS_k3_n100_m423_b30", SAT), ("_39_CBS__CBS_k3_n100_m423_b50", SAT), ("_40_CBS__CBS_k3_n100_m423_b70", SAT), ("_41_CBS__CBS_k3_n100_m423_b90", SAT)]),
+                ("CBS 42 - 51", [("_42_CBS__CBS_k3_n100_m429_b10", SAT), ("_43_CBS__CBS_k3_n100_m429_b30", SAT), ("_44_CBS__CBS_k3_n100_m429_b50", SAT), ("_45_CBS__CBS_k3_n100_m429_b70", SAT), ("_46_CBS__CBS_k3_n100_m429_b90", SAT), ("_47_CBS__CBS_k3_n100_m435_b10", SAT), ("_48_CBS__CBS_k3_n100_m435_b30", SAT), ("_49_CBS__CBS_k3_n100_m435_b50", SAT), ("_50_CBS__CBS_k3_n100_m435_b70", SAT), ("_51_CBS__CBS_k3_n100_m435_b90", SAT)]),
+                ("CBS 52 - 61", [("_52_CBS__CBS_k3_n100_m441_b10", SAT), ("_53_CBS__CBS_k3_n100_m441_b30", SAT), ("_54_CBS__CBS_k3_n100_m441_b50", SAT), ("_55_CBS__CBS_k3_n100_m441_b70", SAT), ("_56_CBS__CBS_k3_n100_m441_b90", SAT), ("_57_CBS__CBS_k3_n100_m449_b10", SAT), ("_58_CBS__CBS_k3_n100_m449_b30", SAT), ("_59_CBS__CBS_k3_n100_m449_b50", SAT), ("_60_CBS__CBS_k3_n100_m449_b70", SAT), ("_61_CBS__CBS_k3_n100_m449_b90", SAT)]),
                 # ("GCP 62 - 69", [
                 #     ("_62_GCP__flat30-60", SAT),
                 #     ("_63_GCP__flat50-115", SAT),
@@ -187,34 +131,35 @@ def print_header():
         print("  -", job)
     print()
 
-def print_before_install():
-    print("      before_install:")
-    print("        - docker build -t cpp .")
-    print("        - docker run --rm --detach --volume `pwd`:/code --workdir /code --name cpp cpp tail -f /dev/null")
-
-def print_after_script():
-    print("      after_script:")
-    print("        - docker stop cpp")
-
-def print_dep(prefix, apt_deps):
+def print_dep(is_docker, apt_deps):
+    prefix = DOCKER_EXEC if is_docker else "sudo"
     if len(apt_deps) is not 0:
         print("      install:")
         print("        -", prefix, "apt update")
         print("        -", prefix, INSTALL_APT_PREFIX, " ".join(apt_deps))
 
+def print_docker_before_install():
+    print("      before_install:")
+    print("        - docker build -t cpp .")
+    print("        - docker run --rm --detach --volume `pwd`:/code --workdir /code --name cpp cpp tail -f /dev/null")
+
+def print_docker_after_script():
+    print("      after_script:")
+    print("        - docker stop cpp")
+
 def print_task(task):
     (stage, is_docker, name, dep, cmds) = task
-    prefix = DOCKER_EXEC if is_docker else ""
     print("    - name:", name)
     print("      stage:", stage)
     if is_docker:
-        print_before_install()
-    print_dep(prefix, dep)
+        print_docker_before_install()
+    print_dep(is_docker, dep)
+    prefix = DOCKER_EXEC if is_docker else ""
     print("      script:")
     for cmd in cmds:
         print("        -", prefix, cmd)
     if is_docker:
-        print_after_script()
+        print_docker_after_script()
 
 def main():
     print_header()
