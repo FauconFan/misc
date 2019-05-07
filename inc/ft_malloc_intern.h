84c6624a94e3d3dc8fd3f56d520aabd25c9a75f9
fauconfan <joseph0priou@gmail.com>
2019-05-08 01:30:00 +0200

    orga + .todo (update)

--- inc/ft_malloc_intern.h
+++ inc/ft_malloc_intern.h
@@ -9,26 +9,30 @@
 #define TINY_MAX    64
 #define SMALL_MAX   3072
 
+#define PAGE_BUFFER 10
+
 typedef struct  s_mlc_block {
     size_t      len;
     uint8_t     is_free;
 }               t_mlc_block;
 
-typedef struct  s_mlc_header {
+// mlc_ph stands for malloc_page_header.
+typedef struct  s_mlc_ph {
     size_t              tot;
-    struct s_mlc_header * next_page;
-}               t_mlc_header;
+    struct s_mlc_ph     * next_page;
+}               t_mlc_ph;
 
 typedef struct  s_mlc_main {
-    int             pgsize;
-    t_mlc_header    * tn_header;
-    t_mlc_header    * sm_header;
-    t_mlc_header    * lrg_header;
+    int         pgsize;
+    t_mlc_ph    * tn_header;
+    t_mlc_ph    * sm_header;
+    t_mlc_ph    * lrg_header;
+    t_mlc_ph    * buffer[PAGE_BUFFER];
 }               t_mlc_main;
 
 // Utils
 
 // mmap_utils.c
-void *mmap_brut(size_t size);
+void *mmap_brut(size_t size); // todelete, create instead a helper function that constructs pages with size multiple of getpagesize
 
 #endif
