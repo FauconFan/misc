b60fbba255bdc8a6dc807e1f92bd910c7e3d7cb9
fauconfan <joseph0priou@gmail.com>
2019-05-12 14:35:07 +0200

    core_loop add comments

--- src/core/core_loop.c
+++ src/core/core_loop.c
@@ -8,10 +8,10 @@ void    core_loop() {
 	dprintf(ui_getfd_log(), "================================== LOOPING ==========================\n");
 	while (running) {
 		dprintf(ui_getfd_log(), "================================== BEGIN LOOP =======================\n");
-		update_buffer();
-		clear_buffer();
+		update_buffer(); // phase 1
+		clear_buffer(); // phase 2
 		g_env->need_neighbours = (lst_size(g_env->li_neighbours) < NB_NEI_MAX);
 		env_print(g_env, ui_getfd_log());
-		running = select_treat_input();
+		running = select_treat_input(); // phase 0
 	}
 } /* client */
