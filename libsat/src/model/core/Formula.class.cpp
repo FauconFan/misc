3c4305431185b63eddb4a62b915490080594eb6a
fauconfan <joseph0priou@gmail.com>
2019-05-29 20:00:27 +0200

    uncrustified it

--- libsat/src/model/core/Formula.class.cpp
+++ libsat/src/model/core/Formula.class.cpp
@@ -96,41 +96,46 @@ Formula Formula::build_false() {
 	return (frml);
 }
 
-static int get_next_var_name (std::set<unsigned int> variables, int n){
+static int get_next_var_name(std::set<unsigned int> variables, int n) {
 	n++;
-	while (variables.find(n) != variables.end()){
+	while (variables.find(n) != variables.end()) {
 		n++;
 	}
 
 	return n;
 }
 
-void Formula::fill_variables(std::set<unsigned int> & current_set) const {
-	switch(this->_type){
-		case Neg : 	this->_u.neg->fill_variables(current_set);
-					break;
-		case Bin :	this->_u.bin.left->fill_variables(current_set);
-					this->_u.bin.right->fill_variables(current_set);
-					break;
-		case Var :	current_set.insert(static_cast<unsigned int>(abs(this->_u.litt)));
-					break;
-		default :	break;
+void Formula::fill_variables(std::set<unsigned int> & current_set) const{
+	switch (this->_type) {
+		case Neg:
+			this->_u.neg->fill_variables(current_set);
+			break;
+		case Bin:
+			this->_u.bin.left->fill_variables(current_set);
+			this->_u.bin.right->fill_variables(current_set);
+			break;
+		case Var:
+			current_set.insert(static_cast<unsigned int>(abs(this->_u.litt)));
+			break;
+		default:
+			break;
 	}
 }
 
-void Formula::formula_to_fnc_rec(Fnc * fnc, int * var_name_ptr, const std::set<unsigned int> variables) const {
+void Formula::formula_to_fnc_rec(Fnc * fnc, int * var_name_ptr, const std::set<unsigned int> variables) const{
 	std::vector<int> clause_vector;
 	int current_var_name;
 
 	current_var_name = *var_name_ptr;
-	*var_name_ptr = get_next_var_name(variables, *var_name_ptr);
+	*var_name_ptr    = get_next_var_name(variables, *var_name_ptr);
 
 	switch (this->_type) {
 		case Neg:
 			int var_name;
-			if (this->_u.neg->_type == Var){
+			if (this->_u.neg->_type == Var) {
 				var_name = this->_u.neg->_u.litt;
-			}else{
+			}
+			else {
 				var_name = *var_name_ptr;
 				this->_u.neg->formula_to_fnc_rec(fnc, var_name_ptr, variables);
 			}
@@ -147,16 +152,18 @@ void Formula::formula_to_fnc_rec(Fnc * fnc, int * var_name_ptr, const std::set<u
 		case Bin:
 			int var_name_left, var_name_right;
 
-			if (this->_u.bin.left->_type == Var){
+			if (this->_u.bin.left->_type == Var) {
 				var_name_left = this->_u.bin.left->_u.litt;
-			}else{
+			}
+			else {
 				var_name_left = *var_name_ptr;
 				this->_u.bin.left->formula_to_fnc_rec(fnc, var_name_ptr, variables);
 			}
 
-			if (this->_u.bin.right->_type == Var){
+			if (this->_u.bin.right->_type == Var) {
 				var_name_right = this->_u.bin.right->_u.litt;
-			}else{
+			}
+			else {
 				var_name_right = *var_name_ptr;
 				this->_u.bin.right->formula_to_fnc_rec(fnc, var_name_ptr, variables);
 			}
@@ -217,7 +224,7 @@ Fnc * Formula::formula_to_fnc() const{
 	fnc = new Fnc();
 
 	this->fill_variables(variables);
-	n = get_next_var_name (variables, n);
+	n = get_next_var_name(variables, n);
 	first_clause_vector.push_back(n);
 	fnc->add_clause(Clause(first_clause_vector));
 
