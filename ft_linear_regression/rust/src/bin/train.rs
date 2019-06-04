4eb769c99661e4dce7033a975b7e4d6b5825d526
fauconfan <joseph0priou@gmail.com>
2019-06-04 11:37:15 +0200

    rust for linear regression

---
+++ ft_linear_regression/rust/src/bin/train.rs
@@ -0,0 +1,83 @@
+use std::env;
+mod utils;
+
+const LEARNING_RATE : f32 = 0.1;
+const OBJECTIVE_ERROR : f32 = 0.0001;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("You must provide a filepath");
+        return
+    }
+
+    let cars = match utils::csv::get_records(&args[1]) {
+        Some(cars) => cars,
+        None => return
+    };
+
+    for car in cars.iter() {
+        println!("{:?}", car);
+    }
+
+    let (norm_car, x_min, x_max, y_min, y_max) = utils::normalization::normalize_vec(&cars);
+
+    println!("x {} {}", x_min, x_max);
+    println!("y {} {}", y_min, y_max);
+
+    for car in norm_car.iter() {
+        println!("{:?}", car);
+    }
+
+    let mut theta0 : f32 = 0.;
+    let mut theta1 : f32 = 0.;
+
+    let mut prev : f32 = error_func(&theta0, &theta1, &norm_car);
+
+    let mut i = 0;
+
+    loop {
+        let mut tmp0 = 0.;
+        let mut tmp1 = 0.;
+
+        for (mileage, price) in norm_car.iter() {
+            tmp0 += theta0 + theta1 * mileage - price;
+            tmp1 += (theta0 + theta1 * mileage - price) * mileage;
+        }
+
+        tmp0 *= 1. / (norm_car.len() as f32);
+        tmp1 *= 1. / (norm_car.len() as f32);
+
+        tmp0 *= LEARNING_RATE;
+        tmp1 *= LEARNING_RATE;
+
+        theta0 -= tmp0;
+        theta1 -= tmp1;
+        let new_error = error_func(&theta0, &theta1, &norm_car);
+        if (new_error - prev).abs() < OBJECTIVE_ERROR {
+            break;
+        }
+        prev = new_error;
+        i += 1;
+    }   
+
+    theta0 = y_min + (y_max - y_min) * (theta0 - theta1 * (x_min) / (x_max - x_min));
+    theta1 = theta1 * (y_max - y_min) / (x_max - x_min);
+
+    println!("Nb tours : {}", i);
+    println!("t0 {} t1 {}", theta0, theta1);
+
+    utils::plot::plot_records_with_linear_line(&cars, Some((theta0, theta1)));
+}
+
+fn error_func(t0 : & f32, t1 : & f32, ve : & Vec<(f32, f32)>) -> f32 {
+    let mut res = 0.0;
+
+    for (x, y) in ve {
+        let tmp = y - (t0 + t1 * x);
+
+        res += tmp * tmp;
+    }
+    return res;
+}
---
+++ ft_linear_regression/rust/src/bin/utils/plot.rs
@@ -0,0 +1,57 @@
+
+extern crate textplots;
+
+const WIDTH_CHART : u32 = 150;
+const HEIGHT_CHART : u32 = 100;
+
+use self::textplots::{Chart, Plot, Shape};
+
+pub fn plot_records(ve : & Vec<(f32, f32)>) {
+    hidden_plot_record(&ve, None);
+}
+
+pub fn plot_records_with_linear_line(ve : & Vec<(f32, f32)>, thetas : Option<(f32, f32)>) {
+    hidden_plot_record(&ve, thetas);
+}
+
+fn hidden_plot_record(ve : & Vec<(f32, f32)>, thetas : Option<(f32, f32)>) {
+    let (mi, ma) = get_min_max_of_x(&ve);
+
+    // default chart
+    Chart::new(WIDTH_CHART, HEIGHT_CHART, mi, ma)
+        .lineplot(Shape::Lines(&ve[..]))
+        .display();
+    
+    // chart with line
+    match thetas {
+        None => (),
+        Some ((t0, t1)) => {
+            Chart::new(WIDTH_CHART, HEIGHT_CHART, mi, ma)
+                .lineplot(Shape::Lines(&ve[..]))
+                .lineplot(Shape::Lines(&[(mi, t0 + t1 * mi), (ma, t0 + t1 * ma)]))
+                .display();
+        }
+    };
+}
+
+fn get_min_max_of_x(ve: & Vec<(f32, f32)>) -> (f32, f32) {
+    let mut result = None;
+
+    for (x, _) in ve {
+        match result {
+            None => {
+                result = Some ((x, x));
+            },
+            Some ((mi, ma)) => {
+                let new_mi = if x < mi { x } else { mi };
+                let new_ma = if x > ma { x } else { ma };
+                result = Some ((new_mi, new_ma));
+            }
+        }
+    }
+
+    return match result {
+        None => (0., 0.),
+        Some ((mi, ma)) => (*mi, *ma),
+    }
+}
