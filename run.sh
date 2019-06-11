54fdaa6b80ee5e5f75e261a7dd071e39d1605b83
Joseph PRIOU <jpriou@e2r6p17.42.fr>
2019-06-11 12:26:14 +0200

    typo ?

--- run.sh
+++ run.sh
@@ -7,12 +7,11 @@ OS=$(uname -s)
 if [ "$OS" = "Darwin" ]; then
     # MacOs
     export DYLD_LIBRARY_PATH=.
-    export DYLD_INSERT_LIBRARIES="libft_malloc.so"
+    export DYLD_INSERT_LIBRARIES="./libft_malloc.so"
     export DYLD_FORCE_FLAT_NAMESPACE=1
 else
     ## Linux
-    export LD_PRELOAD=./libft_malloc.so
+    export LD_PRELOAD="./libft_malloc.so"
 fi
 
 "$@"
-
