b52c40ae06f95c6d85df53223a3935a4a4996d1c
Joseph PRIOU <jpriou@e2r6p17.42.fr>
2019-06-13 09:02:24 +0200

    fix bug check floating point exception

--- src/utils/math_check.c
+++ src/utils/math_check.c
@@ -6,7 +6,7 @@
 /*   By: jpriou <jpriou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/13 08:25:21 by jpriou            #+#    #+#             */
-/*   Updated: 2019/06/13 08:25:21 by jpriou           ###   ########.fr       */
+/*   Updated: 2019/06/13 09:01:29 by jpriou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ t_bool			do_mult_overflow(size_t a, size_t b)
 {
 	size_t	c;
 
+	if (a == 0 || b == 0)
+		return (FALSE);
 	c = a * b;
 	return (c / a != b);
 }
