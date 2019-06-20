95b3c28a0537a9f358f841bd692a5d5d8c2a5ac0
Joseph PRIOU <jpriou@e2r6p17.42.fr>
2019-06-20 18:07:30 +0200

    nm with archive

--- src/ft_nm/ft_nm.c
+++ src/ft_nm/ft_nm.c
@@ -6,21 +6,23 @@
 /*   By: jpriou <jpriou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 08:17:31 by jpriou            #+#    #+#             */
-/*   Updated: 2019/06/20 12:05:28 by jpriou           ###   ########.fr       */
+/*   Updated: 2019/06/20 18:05:40 by jpriou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nm.h"
 
-void		ft_nm(t_ldf *ldf)
+void		ft_nm(t_ldf *ld)
 {
 	uint32_t	magic_number;
 
-	magic_number = *(uint32_t *)ldf->content;
+	magic_number = *(uint32_t *)ld->content;
 	if (magic_number == MH_MAGIC_64)
-		nm_m64(ldf);
+		nm_m64(ld);
 	else if (magic_number == FAT_CIGAM)
-		fat_cigam(ldf, ft_nm);
+		fat_cigam(ld, ft_nm);
+	else if (ld->len >= SARMAG && ft_strncmp(ld->content, ARMAG, SARMAG) == 0)
+		archive(ld, ft_nm);
 	else
 		ft_bput_str_ln(UNKNOWN_FILE);
 }
