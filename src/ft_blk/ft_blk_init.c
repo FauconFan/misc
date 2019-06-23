f5cb91465f57b300562d8760a80c7d3f7c5895d9
Joseph PRIOU <jpriou@e2r6p17.42.fr>
2019-06-23 11:31:23 +0200

    assumption 2^56 to 2^63

--- src/ft_blk/ft_blk_init.c
+++ src/ft_blk/ft_blk_init.c
@@ -6,7 +6,7 @@
 /*   By: jpriou <jpriou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 14:28:38 by jpriou            #+#    #+#             */
-/*   Updated: 2019/06/12 15:25:33 by jpriou           ###   ########.fr       */
+/*   Updated: 2019/06/23 11:19:38 by jpriou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,5 @@ void	ft_blk_init(void *v, size_t remain_tot)
 	head_block = (t_blk *)v;
 	head_block->len_block = remain_tot;
 	ft_blk_set_free(head_block);
-	head_block->len_asked1 = 0;
-	head_block->len_asked2 = 0;
-	head_block->len_asked3 = 0;
+	head_block->len_asked = 0;
 }
--- src/ft_blk/ft_blk_set_occupied.c
+++ src/ft_blk/ft_blk_set_occupied.c
@@ -6,7 +6,7 @@
 /*   By: jpriou <jpriou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 15:21:36 by jpriou            #+#    #+#             */
-/*   Updated: 2019/06/13 12:31:00 by jpriou           ###   ########.fr       */
+/*   Updated: 2019/06/23 11:19:55 by jpriou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,5 @@
 inline void	ft_blk_set_occupied(t_blk *blk, size_t size)
 {
 	blk->free = FALSE;
-	blk->len_asked1 = (uint8_t)(size >> 48);
-	blk->len_asked2 = (uint16_t)(size >> 32);
-	blk->len_asked3 = (uint32_t)(size);
+	blk->len_asked = size;
 }
