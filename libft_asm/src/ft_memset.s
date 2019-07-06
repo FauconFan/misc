bcd741d3d3d12555ebf3adb1d820f17bd99920a0
Joseph PRIOU <jpriou@e2r6p17.42.fr>
2019-07-06 08:19:34 +0200

    good return for memset

--- libft_asm/src/ft_memset.s
+++ libft_asm/src/ft_memset.s
@@ -3,10 +3,18 @@ section .text
 	global ft_memset
 
 ft_memset:
+		push	rbp
+		mov		rbp, rsp
+		sub		rsp, 16
+
+		mov		[rsp + 0], rdi
 		mov		rcx, rdx
 		mov		rax, rsi
 
 		cld
 		rep		stosb
 
+		mov		rax, [rsp + 0]
+		mov		rsp, rbp
+		pop		rbp
 		ret
