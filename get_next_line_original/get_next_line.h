b53a09cd46f2d78c475514e7fc63b0fda220aea5
Joseph PRIOU <jpriou@e2r6p17.42.fr>
2019-08-27 17:46:42 +0200

    add gnl

---
+++ get_next_line_original/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpriou <jpriou@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/12 13:29:17 by jpriou            #+#    #+#             */
+/*   Updated: 2017/11/24 08:46:53 by jpriou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft.h"
+
+# define BUFF_SIZE 			25
+# define MAX_FD				200
+
+int				get_next_line(const int fd, char **line);
+
+#endif
