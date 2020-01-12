da0fb050ea38cff5d7c7c0f72abffb809a914daf
Fauconfan <joseph0priou@gmail.com>
2020-01-12 10:38:29 +0100

    add all pôles

--- src/pages/PresentGeneral.tsx
+++ src/pages/PresentGeneral.tsx
@@ -26,9 +26,49 @@ const PresentGeneral: React.FC = () => {
 		<IonContent className="ion-padding">
 
 			<IonList>
-				<IonItem routerLink='/present/restreint'>
+				<IonItem key='restreint' routerLink='/present/restreint'>
 					<IonLabel>
-					<h2>Pôle restreint</h2>
+					<h2>Bureau restreint</h2>
+					</IonLabel>
+				</IonItem>
+				<IonItem key='anim' routerLink='/present/anim'>
+					<IonLabel>
+					<h2>Pôle Animation</h2>
+					</IonLabel>
+				</IonItem>
+				<IonItem key='inte' routerLink='/present/inte'>
+					<IonLabel>
+					<h2>Pôle Intégration</h2>
+					</IonLabel>
+				</IonItem>
+				<IonItem key='bar' routerLink='/present/bar'>
+					<IonLabel>
+					<h2>Pôle Bar</h2>
+					</IonLabel>
+				</IonItem>
+				<IonItem key='relex' routerLink='/present/relex'>
+					<IonLabel>
+					<h2>Pôle Relex</h2>
+					</IonLabel>
+				</IonItem>
+				<IonItem key='com' routerLink='/present/com'>
+					<IonLabel>
+					<h2>Pôle Communication</h2>
+					</IonLabel>
+				</IonItem>
+				<IonItem key='fip' routerLink='/present/fip'>
+					<IonLabel>
+					<h2>Respo' FIP</h2>
+					</IonLabel>
+				</IonItem>
+				<IonItem key='bdi' routerLink='/present/bdi'>
+					<IonLabel>
+					<h2>Le BDI</h2>
+					</IonLabel>
+				</IonItem>
+				<IonItem key='actif' routerLink='/present/actif'>
+					<IonLabel>
+					<h2>Les membres actifs</h2>
 					</IonLabel>
 				</IonItem>
 			</IonList>
--- src/pages/PresentPole.tsx
+++ src/pages/PresentPole.tsx
@@ -19,9 +19,57 @@ interface PresentPolePageProps extends RouteComponentProps<{
 let mapInformations : Map<string, {'nom': string, 'names': string[], 'description': string}> = new Map();
 
 mapInformations.set('restreint', {
-	'nom' : 'Restreint',
+	'nom' : 'restreint',
 	'names' : ['Augustin Crépin', 'Suzanne Guitton', 'Emma Cariou', 'Paul Marrand', 'Maelle Nicolas'],
-	'description' : "C'est le bureau restreint."
+	'description' : "C'est l'organe central de la liste, le chef de file de cette formidable formation : le Bureau Restreint! Ils sont beaux, compétents et déterminés pour bien mener la vie étudiante du campus!"
+});
+
+mapInformations.set('anim', {
+	'nom' : 'Pôle Anim',
+	'names' : ['Alexandre Morvan', 'Amandine Hervy', 'Hazem Benslimane', 'Niels Haëntjens'],
+	'description' : "Ce corps d'élite en organisation d'événements a été entraîné par les plus grands : le Pôle Anim'! Et ils sont venus à Plouzané pour vous préparer des soirées inoubliables!"
+});
+
+mapInformations.set('inte', {
+	'nom' : 'Pôle Inté',
+	'names' : ['Nicolas Sainte-Rose', 'Pierre Gaillard', 'Kamil Mahmal', 'Maxence Nesme', 'Corentin Herman'],
+	'description' : "Ces valeureux organisateurs de gros événements sont responsables de mettre au point un cursus sans faille pour former la prochaine promotion d'Atlantes : la méthode Pôle Inté'!"
+});
+
+mapInformations.set('bar', {
+	'nom' : 'Pôle Bar',
+	'names' : ['Victor Macé', 'Lucie Lavigne', 'Louis Klein', 'Armand Douce', 'Hugo Ssi Yan Kai'],
+	'description' : "Vague à l'âme, problèmes de coeur ou autres faiblesses de mental? Une équipe de choc est là pour vous accueillir et vous réconforter au bar du foyer : le Pôle Bar!"
+});
+
+mapInformations.set('relex', {
+	'nom' : 'Pôle Relex',
+	'names' : ['Ghislain Lizée', 'Agathe Fernandes', 'Mattéo Chevallier', 'Marie Dubost'],
+	'description' : "Cette fine équipe de chercheurs de trésors vous fera profiter de ses trouvailles, résultats d'âpres recherches et d'aventures palpitantes! Elle est connue sous le nom de Pôle Relex!"
+});
+
+mapInformations.set('com', {
+	'nom' : 'Pôle Com',
+	'names' : ['Nicolas Dieryck', 'Emile Brouard', 'Théo Tembou', 'Corentin Hubert', 'Valéry Boire'],
+	'description' : "Hérauts de la liste, portes-parole de la pensée commune des listeux... Vous les entendez parler souvent! Ils tiennent les Atlantes au courants des différents événements de la vie étudiante! Tendez l'oreille et attendez d'entendre le Pôle Com'!"
+});
+
+mapInformations.set('fip', {
+	'nom' : 'Respo\'FIP',
+	'names' : ['Tanh Huy Le'],
+	'description' : "D'anciennes légendes parlent d'un peuple nomade, connu pour être soudé, et dont les talents de chorale ont traversé les siècles... Cette tribu à l'éléphant rose est représentée par un de ses éminents membres : le Respo'FIP!"
+});
+
+mapInformations.set('bdi', {
+	'nom' : 'BDI',
+	'names' : ['Rayhane Resgui', 'Ismail El Hadrami', 'Rostom Zitoun', 'Luana Maria Siqueira', 'Sami Rmili'],
+	'description' : "A Plouzané on forme l'élite de la Bretagne. Et cette élite est recrutée à l'échelle internationale! Pour représenter ces représentants étrangers, une équipe unique : le BDI!"
+});
+
+mapInformations.set('actif', {
+	'nom' : 'Membres Actifs',
+	'names' : ['Augustin Jaujay', 'Philippine Launay', 'Arthur Doré', 'Florent Thebaut', 'Joseph Priou', 'David Eytan', 'Benjamin Paillette', 'Carla Chevillard', 'Félix Thomann', 'Maëlys Durrieu', 'Marc Tourneux', 'Yasmine Diouri'],
+	'description' : "Aucune liste n'est jamais parvenue à la réussite si elle ne pouvait pas compter sur une base de ferventes et dévouées personnes pour l'aider à surmonter tous les défits de la campagne et de son mandat... Elle peut remercier les Membres Actifs!"
 });
 
 const PresentPole: React.FC<PresentPolePageProps> = ({match}) => {
@@ -51,7 +99,7 @@ const PresentPole: React.FC<PresentPolePageProps> = ({match}) => {
 			<IonList>
 				{data['names'].map((name, index) => {
 					return (
-						<IonItem>
+						<IonItem key={name}>
 							<h3>{name}</h3>
 						</IonItem>
 					);
